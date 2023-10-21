0fd84f259bc7b03a5184838430ea916e58ad1fa8
Eloy Ramirez <clustersnake45@gmail.com>
2023-10-20 22:19:05 -0400

    feat: Cambiar assets y modo de juego

---
+++ scenes/menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_play_pressed():
+#	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+
+func _on_quit_pressed():
+#	pass # Replace with function body.
+	get_tree().quit()
