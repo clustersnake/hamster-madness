3179af1e23d489d20baa77f32071fa25ff632e09
Eloy Ramirez <clustersnake45@gmail.com>
2026-03-11 12:38:32 -0400

    feat: Add new values to constants file

--- hamster-madness/shared/constants.lua
+++ hamster-madness/shared/constants.lua
@@ -37,4 +37,15 @@ M.STATE_STUN = "stun"       -- Aturdido (por golpe o parry)
 M.DAMAGE_NORMAL = 1
 M.DAMAGE_CHARGED = 2        -- Tras rebotes
 
+M.BALL_RANGE_SHOT = 600
+M.BALL_RANGE_EXPELLED = 200
+
+-- shared/constants.lua
+M.HAMSTER_TYPES = {
+	SPEEDY = { speed = 150, can_charge_bounce = false, can_charge_damage = false },
+	TANK = { speed = 80, can_charge_damage = true }, -- Requiere estar quieto
+	TRICKSHOT = { speed = 100, can_charge_bounce = true }, -- Único con rebotes cargados
+	-- ... etc
+}
+
 return M
