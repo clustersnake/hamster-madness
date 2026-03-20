f9c752f28dda2ad13cd4f1083f568bd9ebb9b0e8
Eloy Ramirez <clustersnake45@gmail.com>
2026-03-19 23:34:57 -0400

    feat: Add waves manager

---
+++ Hamster Madness/main/managers/wave_manager.go
@@ -0,0 +1,10 @@
+components {
+  id: "wave_manager"
+  component: "/main/managers/wave_manager.script"
+}
+embedded_components {
+  id: "chaser_factory"
+  type: "factory"
+  data: "prototype: \"/main/enemies/chaser.go\"\n"
+  ""
+}
